21a07c095dcec41379f846923502984f09fb6bd5
wkm <813272417@qq.com>
2017-12-21 21:46:50 +0800

    Spring Cloud Eureka 注册发现中心 增加 spring boot security 认证 增加 禁用自我保护模式 独立单节点Server与集群Server的配置

--- src/main/java/com/nsntc/eureka/EurekaServerApplication.java
+++ src/main/java/com/nsntc/eureka/EurekaServerApplication.java
@@ -19,5 +19,7 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+		/** 通过JVM: -Dport=?传递参数 */
+		//System.getProperty("port");
 	}
 }
